aac1f70b3aab041943d559b4d57579b9ade5424f
Szabó Zsolt <szab.zsolt32@gmail.com>
2026-06-04 20:34:45 +0200

    Bugok javítása!

---
+++ src/main/java/org/example/todoapp/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.todoapp;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(ToDoApplication.class, args);
+    }
+}
---
+++ src/main/java/org/example/todoapp/Task.java
@@ -0,0 +1,27 @@
+package org.example.todoapp;
+
+public class Task {
+    private String name;
+    private boolean done;
+
+    public Task(String name) {
+        this.name = name;
+        this.done = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
